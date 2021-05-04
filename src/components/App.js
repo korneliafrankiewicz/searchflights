73f21ad52b8d5d38b531c529b18f9c750776e20a
korneliafrankiewicz <nelcia6991@gmail.com>
2021-05-04 12:01:04 +0200

    FlightInfo Component and API added

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import { findByPlaceholderText } from "@testing-library/dom";
+
+
+import {FlightInfo} from "./FlightInfo";
+
+const App =() => <FlightInfo/>
+
+
+// function App() {
+//   function handleClick(){
+//     console.log("siema")
+//   }
+
+//   return(
+//     <>
+//     <Button type="warning" text="Counter" onClick = {handleClick}/>
+//     <Button type="info" text="Counter" onClick = {handleClick}/>
+//     <Button type="error" text="Counter" onClick = {handleClick}/>
+//     <Button type="success" text="Counter" onClick = {handleClick}/>;
+//     </> 
+//   )
+
+// }
+
+export default App;
